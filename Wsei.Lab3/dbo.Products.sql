eb213d63c8805cedc5ed079af6722aaa49c63b78
TomaszS00 <tomasz.sadlik00@gmail.com>
2021-12-22 21:42:52 +0100

    update. new db. still issue with data type

---
+++ Wsei.Lab3/dbo.Products.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE [dbo].[Products] (
+    [Id]          INT            IDENTITY (1, 1) NOT NULL,
+    [Name]        NVARCHAR (MAX) NULL,
+    [Description] NVARCHAR (MAX) NULL,
+    [IsVisible]   BIT            NOT NULL,
+    [OwnerId]     NVARCHAR (450) NULL,
+    [Price]       FLOAT (53)     DEFAULT ((0.0000000000000000e+000)) NOT NULL,
+    CONSTRAINT [PK_Products] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_Products_AspNetUsers_OwnerId] FOREIGN KEY ([OwnerId]) REFERENCES [dbo].[AspNetUsers] ([Id])
+);
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_Products_OwnerId]
+    ON [dbo].[Products]([OwnerId] ASC);
+
